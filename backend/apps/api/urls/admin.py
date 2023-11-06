471cf64bf4cc8f8d8b3eab23f8d0f3c11baa9e0b
amirsalmanii <salmaniamir1377@gmail.com>
2023-11-06 12:54:48 +0330

    url book app added

--- backend/apps/api/urls/admin.py
+++ backend/apps/api/urls/admin.py
@@ -1,5 +1,10 @@
 from django.urls import path
 from apps.category.views.admin import CategoryApiViewAdmin, CategoryByIdApiViewAdmin
+from apps.books.views.admin import (
+    BookApiViewAdmin,
+    BookGallerySpiView,
+    BookByIdApiViewAdmin,
+)
 
 urlpatterns = [
     path("category/", CategoryApiViewAdmin.as_view(), name="category_admin_api_view"),
@@ -8,4 +13,7 @@ urlpatterns = [
         CategoryByIdApiViewAdmin.as_view(),
         name="category_admin_by_id",
     ),
+    path("book/", BookApiViewAdmin.as_view(), name="book_admin_api_view"),
+    path("book/gallery/", BookGallerySpiView.as_view(), name="book_gallery_api_view"),
+    path("book/<int:pk>/", BookByIdApiViewAdmin.as_view(), name="book_admin_api_view"),
 ]
--- backend/apps/api/urls/front.py
+++ backend/apps/api/urls/front.py
@@ -5,7 +5,11 @@ from rest_framework_simplejwt.views import (
     TokenVerifyView,
 )
 from apps.category.views.front import CategoryApiViewFront, CategoryByIdApiViewFront
-
+from apps.books.views.front import (
+    FileUploadApiView,
+    BookApiViewFront,
+    BookByIdApiViewFront,
+)
 
 urlpatterns = [
     path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
@@ -16,5 +20,12 @@ urlpatterns = [
         "category/<int:pk>",
         CategoryByIdApiViewFront.as_view(),
         name="category_front_by_id",
+    ),
+    path("file/upload/", FileUploadApiView.as_view(), name="file_upload_view"),
+    path("book/", BookApiViewFront.as_view(), name="book_front_api_view"),
+    path(
+        "book/<int:pk>",
+        BookByIdApiViewFront.as_view(),
+        name="book_front_by_id_api_view",
     ),
 ]
